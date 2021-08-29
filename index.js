f008e066485b623bb90de1c09ae391e1b4220e6d
binaryBrandon <brandon.menninger@gmail.com>
2021-08-29 03:16:05 -0400

    refactored code significantly to use objects instead of multi-dimensional arrays

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ Tail = require('tail').Tail;
 const fileToTail = process.env.LOGFILE;
 tail = new Tail(fileToTail);
 const openStr = '[Mon Aug 23 19:07:13 2021] Udayan tells the guild, \'Opening bids on Pulsing Emerald Hoop\''
-var openItems = [];
+let openItems = [];
 const openRe = /([0-9]{2}\:[0-9]{2}\:[0-9]{2}) .* ([A-Z]{1}[a-z]+?) (?:tells the guild|say to your guild)\, \'[O|o]pening bids on ([A-zA-Z0-9\ \-\,]+)/;
 //[1] is time
 //[2] is person who opened bid
@@ -15,12 +15,14 @@ const openRe = /([0-9]{2}\:[0-9]{2}\:[0-9]{2}) .* ([A-Z]{1}[a-z]+?) (?:tells the
 const bidRe = /([0-9]{2}\:[0-9]{2}\:[0-9]{2}) .* ([A-Z]{1}[a-z]+?) (?:tells the guild|say to your guild)\, \'(.+) (main|ralt|app|fnf|[0-9]+) (main|ralt|app|fnf|[0-9]+)/;
 //[1] is time
 //[2] is person who bid
-//[3] is status or bid
+//[3] is item name
 //[4] is status or bid
+//[5] is status or bid
 const closeRe = /([0-9]{2}\:[0-9]{2}\:[0-9]{2}) .* ([A-Z]{1}[a-z]+?) (?:tells the guild|say to your guild)\, \'([A-zA-Z0-9\ \-\,]+) \>.*sold/;
 //[1] is time
 //[2] is person who closed
 //[3] is item name
+const testRe = /[0-9]{2}\:[0-9]{2}\:[0-9]{2} .* [A-Z]{1}[a-z]+? (?:tells the guild|say to your guild)/;
 
 //in order to interact with findItemIndex() et al, array syntax after any RegEx is called
 //should be [fullString, time, person(, amount/status, amount/status)]
@@ -42,84 +44,139 @@ client.on('messageCreate', message => {
   } else if (message.content === 'list') {
     listItems(message);
   }
-  // else if (message.content === 'stop') {
-  //   message.channel.send('Ending monitor of log file.')
-  //   tail.unwatch();
-  // }
+  else if (message.content === 'stop') {
+    message.channel.send('Ending monitor of log file.')
+    tail.unwatch();
+  }
 });
 
-function readLines(message) {
+const readLines = (message) => {
   tail.on('line', function(data) {
     //console.log(data);
     //message.channel.send(data);
     //[Sat Aug 28 00:28:42 2021] You say, 'jyfkufk'
     //(date)(name)('says to guild')(item name)(status)(bid)
-    if (bidRe.test(data)) {
-      // console.log('working');
-      newBid(message, data);
-    } else if (openRe.test(data)) {
-      openItem(message, data);
-    } else if (closeRe.test(data)) {
-      closeItem(message, data);
+    if (testRe.test(data)) {
+      if (bidRe.test(data)) {
+        // console.log('working');
+        newBid(message, data);
+      } else if (openRe.test(data)) {
+        openItem(message, data);
+      } else if (closeRe.test(data)) {
+        closeItem(message, data);
+      }
     }
     // console.log(result[3]);
     //message.channel.send(`Current bid on ${openItem}: ${bidResult[1]} for ${bidResult[4]} (${bidResult[3]}) `);
-    }
-  );
+  });
 }
 
-function openItem(message, data){
+
+const openItem = (message, data) => {
   let openResult = openRe.exec(data);
-  if (openResult[2] === 'You') {
-    openResult[2] = process.env.YOU;
+  openResult[2] = checkYourself(openResult[2]);
+  // if (openResult[2] === 'You') {
+  //   openResult[2] = process.env.YOU;
+  // }
+  let newItem = {
+    itemName: openResult[3],
+    opener: openResult[2],
+    timeOpened: openResult[1],
+    currentBids: []
   }
-  openItems.push([openResult[3],openResult[2],openResult[1],[]]);
+  openItems.push(newItem);
   //console.log(openItems);
-  message.channel.send(`Bids now open on ${openItems[openItems.length - 1][0]} (opened by ${openItems[openItems.length - 1][1]}).`)
+  message.channel.send(`Bids now open on ${newItem.itemName} (opened by ${newItem.opener}).`)
 }
 
-function listItems(message) {
-  var outString = 'Current items open for bidding:\n';
+const listItems = (message) => {
+  let outString = 'Current items open for bidding:\n';
   //console.log(openItems);
-  for (let i = 0; i < openItems.length; i++){
-    outString += openItems[i][0] + ' — opened by ' + openItems[i][1] + ' at ' + openItems[i][2] + ' ' + process.env.TIMEZONE + '.\n';
-  }
+  // for (let i = 0; i < openItems.length; i++){
+  //   outString += openItems[i][0] + ' — opened by ' + openItems[i][1] + ' at ' + openItems[i][2] + ' ' + process.env.TIMEZONE + '.\n';
+  // }
+
+  openItems.forEach(item => {
+    outString += `${item.itemName} — opened by ${item.opener} at ${item.timeOpened} ${process.env.TIMEZONE}.\n`;
+  });
   message.channel.send(outString);
 }
 
-function closeItem(message, data){
+const closeItem = (message, data) => {
   let closeResult = closeRe.exec(data);
-  if (closeResult[2] === 'You') {
-    closeResult[2] = process.env.YOU;
+  closeResult[2] = checkYourself(closeResult[2]);
+  // if (closeResult[2] === 'You') {
+  //   closeResult[2] = process.env.YOU;
+  // }
+  let targetItemIndex = openItems.findIndex(item => {
+    return item.itemName === closeResult[3];
+  });
+  if (targetItemIndex){
+    // console.log(targetItemIndex);
+    //closeItems.push([openResult[3],openResult[2],openResult[1]]);
+    message.channel.send(`Bids now closed on ${openItems[targetItemIndex].itemName} (closed by ${closeResult[2]}).`);
+    openItems.splice(targetItemIndex, 1);
+  } else {
+    message.channel.send(`${closeResult[2]} tried to close bids on ${closeResult[3]}, but that item was not open (or there was a typo).`);
   }
-  let targetItemIndex = findItemIndex(closeResult[3], data);
-  //closeItems.push([openResult[3],openResult[2],openResult[1]]);
-  message.channel.send(`Bids now closed on ${openItems[targetItemIndex][0]} (closed by ${openItems[targetItemIndex][1]}).`);
-  openItems.splice(targetItemIndex, 1);
 }
 
-function findItemIndex(target, data) {
-  for (let i = 0; i < openItems.length; i++) {
-    if (openItems[i][0] === target) {
-      return i;
-    }
-  }
-  return undefined;
-}
+//not needed since switching to object instead of array of arrays of arrays of arrays of arrays of arrays of...etc.
+//also delete Ames IRL
+// function findItemIndex(target, data) {
+//   for (let i = 0; i < openItems.length; i++) {
+//     if (openItems[i][0] === target) {
+//       return i;
+//     }
+//   }
+//   return undefined;
+// }
 
-function newBid(message, data) {
+const newBid = (message, data) => {
   let bidResult = bidRe.exec(data);
-  if (bidResult[2] === 'You') {
-    bidResult[2] = process.env.YOU;
-  }
-  let targetItemIndex = findItemIndex(bidResult[3], data);
+  bidResult[2] = checkYourself(bidResult[2]);
+  // if (bidResult[2] === 'You') {
+  //   bidResult[2] = process.env.YOU;
+  // }
+  let targetItemIndex = openItems.findIndex(item => {
+    return item.itemName === bidResult[3];
+  });
+  let temp;
   //standardize order: bidder, bid amt, app status, time of bid
-  if (Number(bidResult[3]) === NaN) {//if user put item, status, bid
-    openItems[targetItemIndex][3].push([bidResult[2],bidResult[4],bidResult[3],bidResult[1]]);
-  } else if (Number(bidResult[4]) === NaN) {//if user put item, bid, status
-    openItems[targetItemIndex][3].push([bidResult[2],bidResult[3],bidResult[4],bidResult[1]]);
+  if (Number(bidResult[4]) === NaN) {//if user put item, bid, status
+    temp = bidResult[3];
+    bidResult[3] = bidResult[4];
+    bidResult[4] = temp;
   }
+  //check if this person has bid on this item before
+  // for (let i = 0; i < openItems[targetItemIndex][3].length; i++) {
+  //   if (openItems[targetItemIndex][3][i] === bidResult[2]){
+  //     openItems[targetItemIndex][3][i] = [bidResult[2],bidResult[4],bidResult[3],bidResult[1]];
+  //   }
+  // }
+  let foundItem = openItems[targetItemIndex]
+  let bidderIndex = foundItem.currentBids.findIndex(bid => bid.name === bidResult[2]);
+  if (bidderIndex) {
+    //add new bid object
+    foundItem.currentBids.push(
+      {
+        bidderName: bidResult[2],
+        bidAmount: bidResult[3],
+        bidderStatus: bidResult[4],
+        bidTime: bidResult[5]
+      });
+  } else {
+    //else update their bid
+    let bidder = foundItem.currentBids[bidderIndex];
+    bidder.bidAmount = bidResult[3];
+    bidder.bidAmount = bidResult[5];
+  }
+}
 
+const checkYourself = (result) => {
+  if (result === 'You') {
+    return process.env.YOU;
+  } else return result;
 }
 
 // async function initUnwatch(){
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
-Bids must be opened in the format of /gu Opening bids on itemName
-One item per message
-Bids must be made in the format of /gu itemName [bidAmount or membershipStatus] [bidAmount or membershipStatus]
-One item per message
-Bids must be closed in the format of /gu itemname > [anything] sold [anything]
-One item per message
-"# discord-dkp-tracker"
-Create a .env file and place it in the dkp-bid-tracker folder. This should contain TOKEN=yourDiscordOAuthToken,
-LOGFILE=pathToLogFile, YOU=yourCharName, and TIMEZONE=yourTimeZone. These variables are used to help make the
-code more portable so anyone can run it.
+This Discord bot can be set up and installed by anyone, probably! I don't know, I've never done this before. The assumption is that this repository can be downloaded and run on any local machine, but you must first set up a Discord bot to obtain a unique bot token ID. I may change this later, but for now I'm just trying to get it up and running by Sunday's raid.
+
+-Bids must be opened in the format of /gu Opening bids on itemName
+  -One item per message
+-Bids must be made in the format of /gu itemName [bidAmount or membershipStatus] [bidAmount or membershipStatus]
+  -One item per message
+-Bids must be closed in the format of /gu itemname > [anything] sold [anything]
+  -One item per message
+
+Create a .env file and place it in the dkp-bid-tracker folder. This should contain TOKEN=yourDiscordOAuthToken, LOGFILE=pathToLogFile, YOU=yourCharName, and TIMEZONE=yourTimeZone. These variables are used to help make the code more portable so anyone can run it.
+
+Send a tell to Aradune.Moog or Erollisi.Noskemach and say hi. Long live <Qeynos Militia>.
